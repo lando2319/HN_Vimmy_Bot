29b05c2dbfe3b138a814318a9ce3ffd53a3a0628
Mike Land <mikepland@gmail.com>
2016-05-16 05:50:03 +0000

    refactor, adds timelog

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var Twitter = require("twitter")
 var CronJob = require('cron').CronJob;
 
 new CronJob('0 * * * * ', function() {
-    serveLogActual()
     fetchTopStories()
 }, null, true, 'America/Chicago');
 
@@ -16,23 +15,18 @@ var client = new Twitter({
 });
 
 function serveLogActual() {
-    var currentdate = new Date(); 
-    var datetime = "HN_Vimmy_Bot running Scan: " + currentdate.getDate() + "/"
-                    + (currentdate.getMonth()+1)  + "/" 
-                    + currentdate.getFullYear() + " @ "  
-                    + currentdate.getHours() + ":"  
-                    + currentdate.getMinutes() + ":" 
-                    + currentdate.getSeconds();
-
-    console.log(datetime)
+    var offset = -5;
+    var currentDateActual = new Date( new Date().getTime() + offset * 3600 * 1000).toUTCString().replace( / GMT$/, "" );
+    console.log("Running HN_Vimmy_Bot Scan: " + currentDateActual);
 }
 
 function tweet(tweetActual) {
     client.post('statuses/update', {status: tweetActual},  function(error, tweet, response){
         if (!error && response.statusCode === 200) {
             console.log("I just Tweeted");
-        } else {
-            console.log(error)
+        } else if (error) {
+            // console.log("Is this going to call??");
+            console.log(error);
         }
     });
 }
@@ -42,6 +36,7 @@ function fetchTopStories() {
         url: "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty",
     json: true
     }, function (error, response, body) {
+        serveLogActual();
         if (!error && response.statusCode === 200) {
             compileTop30Stories(body)
         } else {
